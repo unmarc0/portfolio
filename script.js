ff8ac8f32acd4850fae715149f33281ecbfb0332
Marc Olivé <146013224+unmarc0@users.noreply.github.com>
2025-05-15 12:43:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-AOS.init({
-  duration: 1000,
-  once: true
+ScrollReveal().reveal('.seccion h2, .proyecto, form', {
+  delay: 100,
+  distance: '40px',
+  duration: 800,
+  easing: 'ease-out',
+  origin: 'bottom'
 });
